fe65b067dbbb7411af3982ce6e64eb357da9e3dd
Chloe Grace Moretz <shvetsovvitaliy06@gmail.com>
2018-06-05 21:05:11 +0300

    connected app to google sheets

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios_meal_list' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+    pod 'GoogleAPIClientForREST/Sheets', '~> 1.2.1'
+    pod 'GoogleSignIn', '~> 4.1.1'
+end
--- ios_meal_list/ViewController.swift
+++ ios_meal_list/ViewController.swift
@@ -1,25 +1,116 @@
-//
-//  ViewController.swift
-//  ios_meal_list
-//
-//  Created by Chloe on 05/06/2018.
-//  Copyright © 2018 Chloe. All rights reserved.
-//
-
 import UIKit
+import GoogleSignIn
+import GoogleAPIClientForREST
+
+//https://docs.google.com/spreadsheets/d/1XuwNQ8gYQelkVFhlgLlvz5014dT-RWh-PDUBOG9f5FI/edit#gid=0
 
 class ViewController: UIViewController {
-
+    private let scopes = [kGTLRAuthScopeSheetsSpreadsheetsReadonly]
+    private let service = GTLRSheetsService()
+    
+    
+    private let signInButton: GIDSignInButton = {
+        let button = GIDSignInButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
+        
+        setupGoogleApi()
+        setupViews()
+        
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+            GIDSignIn.sharedInstance().signInSilently()
+        } else {
+            GIDSignIn.sharedInstance().signIn()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    private func setupViews() {
+        
     }
+    
+    
+    private func showMeal() {
+        
+    }
+}
 
 
+extension ViewController: GIDSignInDelegate, GIDSignInUIDelegate {
+    private func setupGoogleApi() {
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+    }
+    
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            
+            requestMeal()
+        }
+    }
+    
+    
+    private func requestMeal() {
+        let sheetId = "1XuwNQ8gYQelkVFhlgLlvz5014dT-RWh-PDUBOG9f5FI"
+        let range = "A3:M34"
+        let query = GTLRSheetsQuery_SpreadsheetsValuesGet
+            .query(withSpreadsheetId: sheetId, range: range)
+        
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: #selector(displayMeal(ticket:finishedWithObject:error:)))
+    }
+    
+    
+    @objc private func displayMeal(ticket: GTLRServiceTicket,
+                             finishedWithObject result : GTLRSheets_ValueRange,
+                             error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error getting data", message: error.localizedDescription)
+            return
+        }
+        
+        guard let rows = result.values, !rows.isEmpty else {
+            showAlert(title: "No data", message: "Emtpy data")
+            return
+        }
+        
+        for row in rows {
+            print("\(row[0])")
+        }
+    }
+}
+
+
+extension ViewController {
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
