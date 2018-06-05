421c52abc68f422a76e17f6ee4dac5402f9e4fa3
Chloe Grace Moretz <shvetsovvitaliy06@gmail.com>
2018-06-05 19:37:18 +0300

    Initial Commit

---
+++ ios_meal_list/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_meal_list
+//
+//  Created by Chloe on 05/06/2018.
+//  Copyright © 2018 Chloe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
